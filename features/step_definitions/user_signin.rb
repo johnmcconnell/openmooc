4a4058ce81cb11f7b99438981026f445dad6461f
johnmcconnell <johnnyillinois@gmail.com>
2014-10-10 10:54:23 -0500

    adding step definitions

---
+++ features/step_definitions/user_signin.rb
@@ -0,0 +1,47 @@
+Given(/^I am on the signin page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter valid creditials$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am on the main page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am signed in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter a bad creditials$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I expect I am on the signin page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see invalid username\/password error$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see reset password link$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I leave the site$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit the main page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I sign out$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am not signed in$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/step_definitions/user_signup.rb
@@ -0,0 +1,27 @@
+Given(/^I am on the signup page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter valid signup creditials$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am on the signup confirmation page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter bad email creditials$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see an email signup error$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter bad password creditials$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a password signup error$/) do
+  pending # express the regexp above with the code you wish you had
+end
