6829c8b1d3683d8fee8f6b00d5af6d7d0a30a522
johnmcconnell <johnnyillinois@gmail.com>
2014-10-17 08:50:06 -0500

    added confirmed user factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
   factory :new_user do
     sequence(:email) { |n| Faker::Internet.email }
     sequence(:password) { |n| Faker::Internet.password(12) }
+
+    factory :confirmed_user do
+      confirmed_at Time.now
+    end
   end
 end
