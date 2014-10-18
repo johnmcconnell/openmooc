69a8997030b8d5796486c457de46fee2379eac0f
johnmcconnell <johnnyillinois@gmail.com>
2014-10-18 18:13:15 -0500

    setup views and controller correctly

---
+++ app/controllers/course_controller.rb
@@ -0,0 +1,22 @@
+class CourseController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'course/new'
+
+  get 'course/create'
+
+  get 'course/update'
+
+  get 'course/edit'
+
+  get 'course/destroy'
+
+  get 'course/index'
+
+  get 'course/show'
+
   devise_for :users
   resources :course
   get 'course/query', to: 'course#query'
