df6d9e0c5f7e61bd4abf3a359937867d26750c6d
johnmcconnell <johnnyillinois@gmail.com>
2014-10-18 23:47:40 -0500

    made courses view nicer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019025115) do
+ActiveRecord::Schema.define(version: 20141019034106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141019025115) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
