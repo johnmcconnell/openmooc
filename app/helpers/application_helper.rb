67214624252e7b369d36431d9c43b4d70d991a1b
johnmcconnell <johnnyillinois@gmail.com>
2014-10-19 11:07:25 -0500

    improved course views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
+require 'nokogiri'
+
 module ApplicationHelper
-  def parse_wiki(text)
-    WikiCloth::Parser.new(data: text).to_html
+  def parse_wiki(wikitext)
+    WikiCloth::Parser.new(data: wikitext).to_html
+  end
+
+  def summary_text(wikitext)
+   truncate Nokogiri::HTML(parse_wiki(wikitext)).text, length: 350
   end
 end
