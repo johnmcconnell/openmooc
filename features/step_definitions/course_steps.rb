8e306b07fd8b2655be30fbb952fbd00fbf3eefeb
johnmcconnell <johnnyillinois@gmail.com>
2014-10-23 10:16:04 -0500

    adding cucumber tests for courses

--- features/step_definitions/course_steps.rb
+++ features/step_definitions/course_steps.rb
@@ -1,3 +1,29 @@
+def course
+  @course ||= create(:course)
+end
+
+def new_course
+  @new_course ||= build(:new_course)
+end
+
+Given(/^I am on an edit course page$/) do
+    visit(edit_course_path(course))
+end
+
+Given(/^I am on the courses page$/) do
+    visit(courses_path)
+end
+
+When(/^I enter new course information$/) do
+    enter_form(new_course)
+end
+
 Then(/^I should be on the courses page$/) do
   expect(current_path).to eq(courses_path)
+end
+
+Then(/^I should see the new course information$/) do
+  new_course.each do |key, value|
+    expect(page).to have(value)
+  end
 end
