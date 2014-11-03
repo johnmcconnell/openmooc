809a3ad303f299b1ec332edcaf59527f40fe778c
johnmcconnell <johnnyillinois@gmail.com>
2014-11-03 11:48:23 -0600

    added course adding new content

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   has_one :page_content
   validates_length_of :description, maximum: 400
   accepts_nested_attributes_for :page_content
-  before_save :set_content
+  after_initialize :set_content
 
   private
 
